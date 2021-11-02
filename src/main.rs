0ea5bf5f6420d672d6480c049b23c57682681b6b
Vellen <vellenvirtual@gmail.com>
2021-11-02 11:10:14 +0300

    Simple echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+    // TODO: add logs
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 { // close condition
+            break; 
+        }
+
+        writer.write_all(line.as_bytes()).await.unwrap();
+        line.clear();
+    }
 }
