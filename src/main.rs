4ffd8177b77765351392a6b2a0d67ed69299a1ea
Vellen <vellenvirtual@gmail.com>
2021-11-05 13:10:12 +0300

    Improved echo-server for multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod test;
+
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
@@ -5,22 +7,43 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let args = std::env::args().collect::<Vec<String>>();
+    let server_conf = args.get(1);
+    let server_conf = match server_conf {
+        None => "127.0.0.1:8080",
+        Some(conf) => conf,
+    };
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-    let (reader, mut writer) = socket.split();
+    run(server_conf).await;
+}
 
-    let mut reader = BufReader::new(reader);
-    // TODO: add logs
-    let mut line = String::new();
+async fn run(config: &str) {
+    let listener = TcpListener::bind(config).await.unwrap();
+    println!("Server has started");
 
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 { // close condition
-            break; 
-        }
+        // Allow multiple connections
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+        tokio::spawn(async move {
+            println!("User {} has connected!", _addr);
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            // TODO: add logs
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    // close condition
+                    println!("User {} has disconected", _addr);
+                    break;
+                }
+
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
